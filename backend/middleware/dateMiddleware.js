bcdeb9187e98d684a27f99dfeda54d1cfe1095d3
Eric Lusk-Jopson <eluskjopson@gmail.com>
2022-12-19 21:06:48 -0800

    date manipulation

--- backend/middleware/dateMiddleware.js
+++ backend/middleware/dateMiddleware.js
@@ -7,15 +7,15 @@ const validateGameDates = (req, res, next) => {
     throw new Error("Make sure both a start date and end date are provided");
   }
 
-  const today = new Date(new Date().setHours(0, 0, 0, 0));
+  // const today = new Date(new Date().setHours(0, 0, 0, 0));
   const startDateMidnight = new Date(startDate);
   const endDateMidnight = new Date(endDate);
 
   // Check that start date is not before today
-  if (today.getTime() - 86400000 > startDateMidnight.getTime()) {
-    res.status(410);
-    throw new Error("start date cannot occur before today");
-  }
+  // if (today.getTime() - 86400000 > startDateMidnight.getTime()) {
+  //   res.status(410);
+  //   throw new Error("start date cannot occur before today");
+  // }
 
   // Check that start date comes before end date
   if (startDateMidnight > endDateMidnight) {
